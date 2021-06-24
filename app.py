905582ad393732cab405413bfc7911e6f4717d96
marjonkasa <86376426+marjonkasa@users.noreply.github.com>
2021-06-23 23:46:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name) + " I like to eat fufu"
+	new_name = str(name) + " like to eat fufu"
 	return render_template('index.html', name=new_name)
 
 
